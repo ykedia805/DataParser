3705a4c379e4549835d1e92b0e476d6bb1f0e564
ykedia805 <ykedia805@student.fuhsd.org>
2019-03-10 18:46:45 -0700

    add ElectionResults class

---
+++ src/ElectionResults.java
@@ -0,0 +1,35 @@
+public class ElectionResults {
+    private double demVotes;
+    private double gopVotes;
+    private double totalVotes;
+
+    public ElectionResults(double demVotes, double gopVotes, double totalVotes) {
+        this.demVotes = demVotes;
+        this.gopVotes = gopVotes;
+        this.totalVotes = totalVotes;
+    }
+
+    public double getDemVotes() {
+        return demVotes;
+    }
+
+    public void setDemVotes(double demVotes) {
+        this.demVotes = demVotes;
+    }
+
+    public double getGopVotes() {
+        return gopVotes;
+    }
+
+    public void setGopVotes(double gopVotes) {
+        this.gopVotes = gopVotes;
+    }
+
+    public double getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(double totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+}
